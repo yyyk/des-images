ad581949cbe08edb08a4e60f44ca06e4dc178556
Yuki Kishino <u.key1203@gmail.com>
2022-05-19 08:54:47 +0200

    feat: Add footer (frontend)

--- frontend/src/shared/components/footer.tsx
+++ frontend/src/shared/components/footer.tsx
@@ -1,5 +1,21 @@
 const Footer = () => {
-  return <footer className="prose w-full mx-auto pb-1">footer here...</footer>;
+  return (
+    <footer className="prose w-full mx-auto pb-1">
+      <ul className="list-none p-0 m-0 flex justify-center">
+        <li className="tooltip tooltip-top" data-tip="coming soon!">
+          discord
+        </li>
+        <li className="mx-4">
+          <a href="https://twitter.com/_y_y_y_k_" target="_blank" rel="noopener noreferrer">
+            twitter
+          </a>
+        </li>
+        <li className="tooltip tooltip-top" data-tip="coming soon!">
+          github
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
--- frontend/src/shared/components/layout.tsx
+++ frontend/src/shared/components/layout.tsx
@@ -1,13 +1,13 @@
 import { ReactNode } from 'react';
+import { useThemeContext } from 'src/shared/contexts/theme';
 import { useWalletContext } from 'src/shared/contexts/wallet';
 import AlertBanner from 'src/shared/components/alertBanner';
-import { useThemeContext } from 'src/shared/contexts/theme';
 import Header from 'src/shared/components/header';
-// import Footer from 'src/shared/components/footer';
+import Footer from 'src/shared/components/footer';
 
 const Layout = ({ children }: { children: ReactNode }) => {
-  const { isInvalidChainId } = useWalletContext();
   const { theme } = useThemeContext();
+  const { isInvalidChainId } = useWalletContext();
 
   return (
     <div data-theme={theme} className="min-h-screen flex flex-col flex-nowrap">
@@ -15,7 +15,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
       <div className="px-3 py-0 flex flex-col flex-nowrap grow">
         <Header />
         <main className="prose w-full mx-auto mt-0 pt-0 pb-14 flex flex-col flex-nowrap grow">{children}</main>
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </div>
   );
