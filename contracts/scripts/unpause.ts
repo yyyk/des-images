d2b83c4bc70cc53a45ae1b523ed4cdddcfde482c
Yuki Kishino <u.key1203@gmail.com>
2022-05-28 17:24:52 +0200

    chore: Update scripts (contract)

---
+++ contracts/scripts/unpause.ts
@@ -0,0 +1,23 @@
+import { ethers } from "hardhat";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+async function main() {
+  if (!process.env.CONTRACT_ADDRESS) {
+    return;
+  }
+
+  const DesImages = await ethers.getContractFactory("DesImages");
+  const desImages = await DesImages.attach(process.env.CONTRACT_ADDRESS);
+  await desImages.unpause();
+  console.log("desImages unpause.");
+
+  const receipt = await desImages.deployTransaction.wait();
+  console.log("gasUsed:", receipt.gasUsed._hex);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
