d8e1fac89342f2330655a3259b1caf95d29fc3a2
Yuki Kishino <u.key1203@gmail.com>
2022-06-01 23:56:16 +0200

    feat: update header component (frontend)

--- frontend/src/shared/components/header.tsx
+++ frontend/src/shared/components/header.tsx
@@ -22,26 +22,61 @@ const ModBadgeLink = ({ toCatalog }: { toCatalog: boolean }) => (
 );
 
 const CatalogLink = () => (
-  <Link className="btn btn-md ml-auto" to="/catalog">
-    to the catalog
-  </Link>
+  <>
+    <Link className="btn btn-square w-[36px] h-[36px] min-h-[36px] ml-auto flex sm:hidden" to="/catalog">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={2}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z"
+        />
+      </svg>
+    </Link>
+    <Link className="btn btn-md ml-auto hidden sm:flex" to="/catalog">
+      to the catalog
+    </Link>
+  </>
 );
 
 const GoBackButton = () => {
   const navigate = useNavigate();
   return (
-    <button className="btn btn-md btn-outline ml-auto" onClick={() => navigate(-1)}>
-      go back
-    </button>
+    <>
+      <button
+        className="btn btn-square w-[36px] h-[36px] min-h-[36px] ml-auto flex sm:hidden"
+        onClick={() => navigate(-1)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M11 17l-5-5m0 0l5-5m-5 5h12" />
+        </svg>
+      </button>
+      <button className="btn btn-md ml-auto hidden sm:flex" onClick={() => navigate(-1)}>
+        go back
+      </button>
+    </>
   );
 };
 
 const WalletAddress = () => {
   const { walletAddress } = useWalletContext();
   return (
-    <span className="ml-auto">{`Connected: ${String(walletAddress).substring(0, 6)}...${String(walletAddress).substring(
-      38,
-    )}`}</span>
+    <span className="hidden sm:inline ml-auto">{`Connected: ${String(walletAddress).substring(0, 6)}...${String(
+      walletAddress,
+    ).substring(38)}`}</span>
   );
 };
 
@@ -55,7 +90,23 @@ const ConnectWalletButton = () => {
   return (
     <>
       <>
-        <button className="btn btn-md ml-auto" onClick={handleClick}>
+        <button className="btn btn-square w-[36px] h-[36px] min-h-[36px] ml-auto flex sm:hidden" onClick={handleClick}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+            />
+          </svg>
+        </button>
+        <button className="btn btn-md ml-auto hidden sm:flex" onClick={handleClick}>
           Connect Wallet
         </button>
         <WalletModal open={walletModalOpen} onClose={() => setWalletModalOpen(false)} />
