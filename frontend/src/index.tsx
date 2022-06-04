2bab0616477bfcff5e95d6480d053afceae81255
Yuki Kishino <u.key1203@gmail.com>
2022-06-04 15:19:49 +0200

    feat: add smooth scroll polyfill (frontend)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import smoothscroll from 'smoothscroll-polyfill';
+
+smoothscroll.polyfill();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
