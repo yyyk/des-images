50a6f95173c2d20f8dc8da3ad7a2317f9c7d13f6
Yuki Kishino <u.key1203@gmail.com>
2022-06-10 00:23:23 +0200

    chore: update carco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -22,6 +22,16 @@ module.exports = {
           Buffer: ['buffer', 'Buffer'],
         }),
       ],
+      ignoreWarnings: [
+        function ignoreSourcemapsloaderWarnings(warning) {
+          return (
+            warning.module &&
+            warning.module.resource.includes('node_modules') &&
+            warning.details &&
+            warning.details.includes('source-map-loader')
+          );
+        },
+      ],
     },
   },
 };
